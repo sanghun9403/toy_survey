4ebf5bd2e9df743991371a978f5fdb1209319ca9
Lee SangHun <mysh1105@gmail.com>
2023-11-18 20:36:39 +0900

    feat : Answer CRUD
    - 기본적인 기능 완성
    - 설문지 생성 시 질문지와 선택지가 같이 생성되도록 코드 변경 예정

--- src/answers/answers.module.ts
+++ src/answers/answers.module.ts
@@ -5,9 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Answer } from 'src/_common/entities/answer.entity';
 import { Question } from 'src/_common/entities/question.entity';
 import { ResponseDetail } from 'src/_common/entities/response-detail.entity';
+import { QuestionsService } from 'src/questions/questions.service';
+import { SurveysService } from 'src/surveys/surveys.service';
+import { Survey } from 'src/_common/entities/survey.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Answer])],
-  providers: [AnswersResolver, AnswersService],
+  imports: [TypeOrmModule.forFeature([Answer, Question, Survey])],
+  providers: [
+    AnswersResolver,
+    AnswersService,
+    QuestionsService,
+    SurveysService,
+  ],
 })
 export class AnswersModule {}
